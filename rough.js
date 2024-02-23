ee597877c8c9364af9cde557c97da4224bbe719d
KKPATNAIK <160569659+KKPATNAIK@users.noreply.github.com>
2024-02-23 14:36:17 +0530

    This is my third commit on this date

---
+++ rough.js
@@ -0,0 +1,18 @@
+const accountUser = "SomeoneAcc"
+let accountEmail = "Somethingjs@gmail.com"
+var accountPassword = "something@0905"
+accountCity = "Mumbai"
+let accountName;
+
+accountEmail = "SomethingElse@gmail.com"
+accountEmail = "myemail.com"
+
+accountPassword = 4940005950
+accountCity = "Dehradun"
+
+console.table(
+            [accountUser, accountEmail, accountPassword, accountCity, accountName]
+
+)
+
+
