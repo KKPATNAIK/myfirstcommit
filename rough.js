f4bad944cde0e8779d6a67b16339876d47dc7e6a
KKPATNAIK <160569659+KKPATNAIK@users.noreply.github.com>
2024-02-25 19:33:54 +0530

    This the commit made by me on 25 feb 24

--- rough.js
+++ rough.js
@@ -16,3 +16,33 @@ console.table(
 )
 
 
+let a = 10;
+b = 20;
+c = 30;
+d= 50;
+e = 40;
+
+console.log(a**2);
+
+function exponent(i,j){
+    console.log(i**j)
+    k=i++
+    console.log("The square of the first number with its next increment number is    :", (i)**(k))
+
+}
+exponent(5,2);
+exponent(6,4);
+exponent(70,1);
+exponent (43, 10);
+
+function sqrs(i,j){
+
+
+}
+
+const myParagraph = document.getElementById('my-paragraph');
+myParagraph.textContent = "The text content of this paragraph is changed by JavaScript!";
+
+
+
+
