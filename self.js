123fcffdfaa88e70a3b3f76b5a2c8feee1c2be2b
KKPATNAIK <160569659+KKPATNAIK@users.noreply.github.com>
2024-02-26 14:10:26 +0530

    This is my commit on 26 Feb 24

--- self.js
+++ self.js
@@ -1,4 +1,121 @@
+
+const someOne = document.getElementById('firstPara');
+if(someOne){
+    someOne.textContent = "This text is changed byJavascript!";
+    someOne.style.color = "red";
+   
+
+}
+
 for (i=1; i<=5; i++ )
+
+
+
+
 {
     console.log("We Protect")
 }
+
+// let userNum = prompt("Enter the number");
+// let guestNum = 25;
+// { if (userNum!=guestNum)
+//    userNum =  console.log(prompt("Enter the number again it's wrong"))
+// }
+// {
+
+ 
+// console.log("Congratulations u have entered corerct value");
+// }
+
+//odd or even of first n numbers
+
+for (i=1; i<=100;i++)
+{
+    if (i%2==0)
+    console.log("The even numbers are:", i);
+else 
+console.log("The odd numbers are: ",i);
+}
+{
+function yuvi(a,b,c){
+if(a>=b && b>=c)
+console.log("The greatest number is: ", a);
+
+else if(b>=a && a>=c )
+console.log("The greatest number is:  ", b);
+
+else (c>=a && a>=b)
+console.log("The greatest number is:  ", c)
+
+
+}}
+
+{
+yuvi(19,18,64);
+}
+{
+yuvi(12,33,77);
+}
+{
+yuvi(20,17,10);
+}
+{
+yuvi(10,9,7);
+}
+{
+yuvi(4,4,2);
+}
+{
+yuvi(15,9,1);
+}
+{
+yuvi(33,0,33);
+}
+{
+yuvi(0,1,0);
+}
+
+let userNam = prompt("Enter your full name without space :");
+{
+    console.log(`Your username is @${userNam}${userNam.length}`)
+}
+let output = 0;
+let sum = prompt("Enter the first Number:");
+
+    let m = prompt("Enter the second number :");
+    output == sum + m;
+{console.log("The sum of the two entered number is :",output);}
+
+function nuMB(n){
+for (i=1; i<=n; ++i){
+    console.log("The numbers are :")
+    console.log(i)
+
+    
+}
+}
+
+nuMB(5);
+
+
+
+
+
+let numBer = prompt("Enter the number :")
+for (i=1; i<=numBer; i++){
+    console.log("The kk numbers are:",i)
+}
+
+
+//To find out the entered number is odd or even
+let oddEven = prompt ("Enter the number to find out wheher the number is odd or even :")
+{if (oddEven%2==0)
+    console.log(`The number ${oddEven} you have entered is even`);
+    else
+console.log(`The number ${oddEven} you have entered is odd `)}
+
+//to print the numbers in tree format starting from 1 to n
+
+
+
+
